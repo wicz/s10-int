8278bc5d3a10a59780732378fd65a3c4fac52ffb
Vinicius Horewicz <vinicius@horewi.cz>
2012-01-19 15:26:26 -0200

    change DATA_DIR for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 require_relative '../lib/wikail'
 
+module Wikail
+  module Environment
+    DATA_DIR = File.expand_path('../data', __FILE__)
+  end
+end
+
 Mail.defaults do
   delivery_method :test
   retriever_method :test
